4e1a42e07d810c3aa26e673d619ffe78ba806eab
Saeed <saeed_et90@yahoo.com>
2021-11-22 20:09:55 +1100

    login page design added

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,41 @@
 import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { Button, Grid, Snackbar, TextField } from "@material-ui/core";
+import { Button, Grid, Paper, Snackbar, TextField } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import { useUser } from "../context/AuthContext";
 import { Auth } from "aws-amplify";
 import { CognitoUser } from "@aws-amplify/auth";
 import { useRouter } from "next/router";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 interface IFormInput {
   username: string;
   password: string;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formContainer: {
+      width: "350px",
+      marginBottom: 32,
+      marginTop: "40px",
+    },
+    fullWidth: {
+      width: "100%",
+    },
+    btnHolder: {
+      width: "96%",
+    },
+    inputBg: {
+      ":-webkit-autofill": {
+        backgroundColor: "red !important",
+      },
+    },
+  })
+);
+
 export default function Login() {
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
   const router = useRouter();
   const [signInError, setSignInError] = useState<string>("");
@@ -42,45 +65,63 @@ export default function Login() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-        <Grid container direction="column" alignItems="center" justify="center">
-          <Grid item>
-            <TextField
-              variant="outlined"
-              id="username"
-              label="Username"
-              type="text"
-              error={errors.username ? true : false}
-              helperText={errors.username ? errors.username.message : null}
-              {...register("username")}
-            />
-          </Grid>
+    <Grid container alignItems="center" justifyContent="center">
+      <Paper elevation={20} className={classes.formContainer}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          autoComplete="off"
+          style={{ margin: "20px" }}
+        >
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justify="center"
+            spacing={2}
+          >
+            <Grid item className={classes.fullWidth}>
+              <TextField
+                className={classes.fullWidth}
+                variant="outlined"
+                id="username"
+                label="Username"
+                type="text"
+                error={errors.username ? true : false}
+                helperText={errors.username ? errors.username.message : null}
+                {...register("username")}
+              />
+            </Grid>
 
-          <Grid item>
-            <TextField
-              variant="outlined"
-              id="password"
-              label="Password"
-              type="password"
-              error={errors.password ? true : false}
-              helperText={errors.password ? errors.password.message : null}
-              {...register("password")}
-            />
-          </Grid>
+            <Grid item className={classes.fullWidth}>
+              <TextField
+                className={classes.fullWidth}
+                variant="outlined"
+                id="password"
+                label="Password"
+                type="password"
+                error={errors.password ? true : false}
+                helperText={errors.password ? errors.password.message : null}
+                {...register("password")}
+              />
+            </Grid>
 
-          <Grid style={{ marginTop: 16 }}>
-            <Button variant="contained" type="submit">
-              Sign In
-            </Button>
+            <Grid style={{ marginTop: 16 }} className={classes.btnHolder}>
+              <Button
+                variant="contained"
+                type="submit"
+                className={classes.fullWidth}
+              >
+                Sign In
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </form>
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="error">
-          {signInError}
-        </Alert>
-      </Snackbar>
-    </>
+        </form>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error">
+            {signInError}
+          </Alert>
+        </Snackbar>
+      </Paper>
+    </Grid>
   );
 }
